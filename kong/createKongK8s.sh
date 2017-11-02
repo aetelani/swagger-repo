c05871fdb8fe203e7190a278815133d41bf8f9af
Anssi Eteläniemi <anssi.etelaniemi@liaison.com>
2017-11-02 11:33:10 +0200

    Removed original kong kube subtree ref

--- kong/createKongK8s.sh
+++ kong/createKongK8s.sh
@@ -3,6 +3,6 @@ kubectl create -f postgres.yaml
 kubectl create -f kong_migration_postgres.yaml
 kubectl delete -f kong_migration_postgres.yaml
 kubectl create -f kong_postgres.yaml
-kubectl get all
+kubectl get all --selector=app=kong
 #curl $(kubectl srv --url kong-admin)
 #curl $(kubectl srv --url kong-proxy|head -n1)
--- kong/deleteKongK8s.sh
+++ kong/deleteKongK8s.sh
@@ -1,3 +1,5 @@
 cd kong-dist-kubernetes.git || exit 1
 kubectl delete -f kong_postgres.yaml
 kubectl delete -f postgres.yaml
+echo "Still running resources..."
+kubectl get all -o=wide --selector=app=kong -ojson
