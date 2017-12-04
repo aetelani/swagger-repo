29815b8fda5db7d4fa90ce496d85829b571cf95c
aetelani <aetelani@hotmail.com>
2017-12-04 14:28:21 +0000

    Generated code by SwaggerHub

---
+++ nodejs-server/service/DefaultService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * Get the Weather
+ * Get the Weather from wherever you want
+ *
+ * no response value expected for this operation
+ **/
+exports.weatherGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
