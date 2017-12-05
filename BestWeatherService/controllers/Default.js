55bb19b320445b42672049c4869959aff5f62d00
aetelani <aetelani@hotmail.com>
2017-12-05 11:51:28 +0000

    Generated code by SwaggerHub

--- BestWeatherService/controllers/Default.js
+++ BestWeatherService/controllers/Default.js
@@ -3,9 +3,9 @@
 var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
 
-module.exports.tre = function tre (req, res, next) {
+module.exports.withLocation = function withLocation (req, res, next) {
   var location = req.swagger.params['location'].value;
-  Default.tre(location)
+  Default.withLocation(location)
     .then(function (response) {
       utils.writeJson(res, response);
     })
